22e6b5647c30333c96ca6ea4b7b9b4a18ac734cd
jay <jaypifiverr@gmail.com>
2022-07-04 01:08:04 -0400

    commit tree 8ce79a7019a2b0adf6378b48d43b022e5a82dc46

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code your solution here
+const record = [
+
+    { 
+        year: "2015", 
+        result: "W"
+    },
+    { 
+        year: "2014", 
+        result: "N/A"
+    },
+    { 
+        year: "2013", 
+        result: "L"
+    },
+  ]
+// function superbowlWin(array) {
+//     array === "W"
+//     return array.year
+// }
+
+const yearNon = record.find(superBowlNone)
+
+function superbowlWin(game){
+    for (const item of game)
+    if (item.result === "W"){
+    return item.year
+    }
+}
+
+function superBowlNone(game){
+    if (game.result === "N/A")
+    return undefined
+}
+
